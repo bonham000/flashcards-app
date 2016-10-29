0ec97503d4334d979bd07dd322f06c2f36f76e7a
Sean Smith <sean.smith.2009@gmail.com>
2016-10-29 16:16:36 -0700

    updated port

--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,17 @@
 import express from 'express';
 import path from 'path';
 import fallback from 'express-history-api-fallback';
-import devConfig from './config/setup/dev';
-import prodConfig from './config/setup/prod';
+//import devConfig from './config/setup/dev';
+//import prodConfig from './config/setup/prod';
 import { NODE_ENV } from './config/env';
 
 const app = express();
 
-if (NODE_ENV === 'development') {
-  devConfig(app);
-} else {
-  prodConfig(app);
-}
-
-app.get('/page', (req, res) => {
-	res.send('this is a demo alternate route');
-});
+// if (NODE_ENV === 'development') {
+//   devConfig(app);
+// } else {
+//   prodConfig(app);
+// }
 
 app.use(express.static('dist/client'));
 app.use(fallback(path.join(__dirname, '../../dist/client/index.html')));
